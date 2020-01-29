a6f32eaef9069c28f2d379056d1164b29f224ef1
Sathiya Sangar <34126794+sathiyasangar@users.noreply.github.com>
2020-01-29 10:48:01 +0530

    Delete mulchi.py

--- Inheritance/multiple inheritance/mulchi.py
+++
@@ -1,9 +0,0 @@
-from mulpar1 import Parent1
-from mulpar2 import Parent2
-
-class Child(Parent1, Parent2):  
-    pass
-ob = Parent1("Parent1 Success..!")
-ob.show()
-obs = Parent2("Parent2 Success..!")
-obs.shown()
